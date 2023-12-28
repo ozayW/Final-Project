cb2adca4b22dd1e422cc734e4400a989f7c17496
oz <ozmoalem4@gmail.com>
2023-12-28 17:17:12 +0200

    Version 1.01

--- Server/DBHandle.py
+++ Server/DBHandle.py
@@ -2,19 +2,27 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://ozay:<password>@cluster0.qjdp42h.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://ozay:Oz_0586618917@cluster0.qjdp42h.mongodb.net/?retryWrites=true&w=majority"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+def add_user(username, password, role='None'):
+    with MongoClient(uri) as cluster:
+        users = cluster['GYM']['Users']
+        if(not users.find_one({'Username': username})):
+            users.insert_one({'Username':username, 'Password':password, 'Role':role})
+            return True
+        return False
 
-with MongoClient(uri) as cluster:
-    users = cluster['GYM']['Users']
-    user1 = users.find_one({'Username': 'Ozay'})
-    print(user1)
+def upadte_user(username, field, new_data):
+    try:
+        with MongoClient(uri) as cluster:
+            users = cluster['GYM']['Users']
+            users.update_one({'Username':username}, {'$set':{field, new_data}})
+            user = users.find_one({'Username':username})
+            if(user[field] == new_data)
+                return True
+            return False
+    except:
+        return False
