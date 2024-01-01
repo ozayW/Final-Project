b6b87257178040b1bd7534567dba0f77bc143eac
oz <ozmoalem4@gmail.com>
2024-01-01 17:37:57 +0200

    version 1.02

--- Server/DBHandle.py
+++ Server/DBHandle.py
@@ -7,23 +7,75 @@ uri = "mongodb+srv://ozay:Oz_0586618917@cluster0.qjdp42h.mongodb.net/?retryWrite
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
-def update_amount(username):
+#**Updates Database**#
+def add_workout_update(username, date, pullups, one_arm_pullups, deadhang, spinning_bar_deadhang, lashe):
+    with MongoClient(uri) as cluster:
+        updates = cluster['GYM']['Updates']
+        if not workout_update_exists(username, date):
+            updates.insert_one({'Username': username, 'Date': date, 'Pull-Ups': pullups,
+                                'One Arm Pull-Ups': one_arm_pullups, 'Deadhang': deadhang,
+                                'Spinning Bar Deadhang': spinning_bar_deadhang, 'Lashe': lashe})
+            return True
+        return False
+def get_from_workout_update(username, date, field):
+    with MongoClient(uri) as cluster:
+        updates = cluster['GYM']['Updates']
+        update = updates.find_one({'Username': username, 'Date':date})
+        return update.get(field)
+
+def workout_update_exists(username, date):
+    with MongoClient(uri) as cluster:
+        updates = cluster['GYM']['Updates']
+        if updates.find_one({'Username': username, 'Date': date}):
+            return True
+        return False
+
+
+
+#**Workouts Database**#
+
+def add_workout(date, timeslot, trainer, level, trainees, max_num_of_trainees, pending):
+    with MongoClient(uri) as cluster:
+        workouts = cluster['GYM']['Workouts']
+        if not workout_exists(date, timeslot):
+            workouts.insert_one({'Date': date, 'Time-slot':timeslot, 'Trainer':trainer, 'Level':level, 'Trainees':trainees,
+                             'Max Number Of Trainees': max_num_of_trainees, 'pending': pending})
+            return True
+        return False
+
+def add_trainee(date, timeslot, username):
+    with MongoClient(uri) as cluster:
+        workouts = cluster['GYM']['Workouts']
+        if workout_exists(date, timeslot):
+            max_num_of_trainees = get_from_workout(date, timeslot, 'Max Number Of Trainees')
+            trainees = get_from_workout(date, timeslot, 'Trainees')
+            trainees.append(username)
+            if len(trainees) < max_num_of_trainees:
+                workouts.update_one({'Date': date, 'Time-slot': timeslot}, {'$set': {'Trainees': trainees}})
+                return True
+        return False
+def get_from_workout(date, timeslot, field):
+    with MongoClient(uri) as cluster:
+        workouts = cluster['GYM']['Workouts']
+        workout = workouts.find_one({'Date':date, 'Time-Slot':timeslot})
+        return workout.get(field)
+
+def workout_exists(date, timeslot):
+    with MongoClient(uri) as cluster:
+        workouts = cluster['GYM']['Workouts']
+        if workouts.find_one({'Time-Slot': timeslot, 'Date': date}):
+            return True
+        return False
+
+
+
+#***Users Database**#
+def add_user(username,role, password, level, max_workouts):
     with MongoClient(uri) as cluster:
         users = cluster['GYM']['Users']
-        if(not users.find_one({'Username' : username})):
-            user = users.find_one({'Username':username})
-            i = 1
-            field = 'Workout Update ' + str(i)
-            while  user.get(field) is not None:
-                i+=1
-                field = 'Workout Update ' + str(i)
-            return i
-        return 0
-def add_user(username, password, level, max_workouts, workout_update, role='None'):
-    with MongoClient(uri) as cluster:
-        users = cluster['GYM']['Users']
-        if(not users.find_one({'Username': username})):
-            users.insert_one({'Username':username, 'Password':password, 'Role':role})
+        if not users.exists(username):
+            users.insert_one({'Username':username, 'Password':password, 'Role':role,
+                                'Level':level, 'MaxWorkouts':max_workouts})
             return True
         return False
 
@@ -31,11 +83,23 @@ def upadte_user(username, field, new_data):
     try:
         with MongoClient(uri) as cluster:
             users = cluster['GYM']['Users']
-            users.update_one({'Username':username}, {'$set':{field, new_data}})
+            users.update_one({'Username':username}, {'$set': {field, new_data}})
             user = users.find_one({'Username':username})
-            if(user[field] == new_data)
+            if user[field] == new_data:
                 return True
             return False
     except:
         return False
 
+def get_from_user(username, field):
+    with MongoClient(uri) as cluster:
+        users = cluster['GYM']['Users']
+        user = users.find_one({'Username':username})
+        return user.get(field)
+
+def user_exists(username):
+    with MongoClient(uri) as cluster:
+        users = cluster['GYM']['Users']
+        if users.find_one({'Username': username}):
+            return True
+        return False
---
+++ Server/Server.py
@@ -0,0 +1,30 @@
+import socket
+import threading
+
+SERVER_IP = '127.0.0.1'
+SERVER_PORT = '6090'
+clients_objects = []
+
+def init_server():
+    server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server_socket.bind((SERVER_IP,SERVER_PORT))
+    server_socket.listen()
+    print(" waiting for clients")
+    return server_socket
+
+
+def act(action, client):
+    pass
+
+def main():
+    server_socket = init_server()
+    while True:
+        client_object, client_IP = server_socket.accept()
+        clients_objects.append(client_object)
+        client_th = threading.Thread(target=act, args=('action',client_object))
+        client_th.start()
+
+
+
+if __name__ == "__main__":
+    main()
